59ae5300cdf40ff73b65fe8c9e58a5934eaef1d4
Christine De Padua <tinadepadua19@gmail.com>
2026-07-23 20:21:39 +0800

    commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import { getToken, clearToken } from './services/api';
+import * as authService from './services/authService';
+
+export default function App() {
+
+}
