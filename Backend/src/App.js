1205134bd964e985abbfdb545a336ab1975b83cd
T I N E <tinadepadua19@gmail.com>
2026-09-04 10:32:38 +0800

    Update App.js

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -23,7 +23,10 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://capstone-frontend-lake.vercel.app'
+    'https://capstone-frontend-lake.vercel.app',
+    "https://aileencakemax.tech",
+    "https://www.aileencakemax.tech"
+
   ],
   credentials: true // kailangan ito dahil gumagamit ka ng withCredentials sa axios
 }));
